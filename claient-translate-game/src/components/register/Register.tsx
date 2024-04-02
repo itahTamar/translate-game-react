926663ebcd2a02e1608965589489d66af4984bea
itahTamar <tamar.verber@gmail.com>
2024-04-02 16:01:44 +0300

    register form

---
+++ claient-translate-game/src/components/register/Register.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { register } from "../../api/users/userApi";
+
+const Register = () => {
+  const [userName, setUserName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const navigate = useNavigate();
+
+  const handleSubmitRegister = async (ev: React.FormEvent<HTMLFormElement>) => {
+    try {
+      ev.preventDefault();
+      const data = { userName, password };
+      if (!data) throw new Error("register failed - no email or password");
+      const ok = await register(userName, password);
+      if (!ok) throw new Error("register failed from server");
+      navigate("/landingPage");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <div className="login-container">
+        <form className="login-form" onSubmit={handleSubmitRegister}>
+          <div className="input_container">
+            <div className="icon_container"></div>
+            <input
+              type="userName"
+              name="userName"
+              autoComplete="given-name"
+              placeholder="UserName (must be uniq)"
+              value={userName}
+              onInput={(ev) =>
+                setUserName((ev.target as HTMLInputElement).value)
+              }
+            />
+          </div>
+          <div className="input_container">
+            <div className="icon2_container"></div>
+            <input
+              type="password"
+              name="password"
+              autoComplete="off"
+              placeholder="Password"
+              value={password}
+              onInput={(ev) =>
+                setPassword((ev.target as HTMLInputElement).value)
+              }
+            ></input>
+          </div>
+          <button
+             type="submit"
+          >
+            Register
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Register;
--- claient-translate-game/src/view/RegisterPage.tsx
+++ claient-translate-game/src/view/RegisterPage.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import Register from '../components/register/Register'
 
 const RegisterPage = () => {
   return (
-    <div>
-      register
-    </div>
+    <Register/>
   )
 }
 
