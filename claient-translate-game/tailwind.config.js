ec77df00c17689549180ae81a9c7299182b90cfa
itahTamar <tamar.verber@gmail.com>
2024-05-20 19:17:50 +0300

    connect tailwind

--- claient-translate-game/tailwind.config.js
+++ claient-translate-game/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  // ...
 }
 
