514c4d59a1e96f84a53780724a73f04bbf7e0831
itahTamar <tamar.verber@gmail.com>
2024-06-18 19:36:40 +0300

    style landing page

--- claient-translate-game/src/view/LandingPage.tsx
+++ claient-translate-game/src/view/LandingPage.tsx
@@ -3,35 +3,41 @@ import { useNavigate } from "react-router-dom";
 import Popup from "./../components/popup";
 import Login from "./../components/login/Login";
 import "../style/buttons.css";
+import "../style/images/bgImg.css";
 
 const LandingPage = () => {
   const navigate = useNavigate();
   const [showPopupLogin, setShowPopupLogin] = useState(false);
 
   return (
-    <div>
-      <h1>Vocabulary Game</h1>
-      <div className="p-12">
-        <button className="loginLP" onClick={() => setShowPopupLogin(true)}>
-          Log in
-        </button>
-        {showPopupLogin && (
-          <Popup onClose={() => setShowPopupLogin(false)}>
-            <Login />
-          </Popup>
-        )}
-        <p className="p-2">or</p>
-        <button
-          className="RegisterFirst"
-          onClick={() => {
-            navigate(`/register`);
-          }}
-        >
-          Register
-        </button>
-      </div>
+    <div className="open-page">
+      <div className="container">
+        <h1 className="text-7xl mb-20 font-bold">Vocabulary Game</h1>
+        <h2 className="text-3xl mb-5">Practice your vocabulary</h2>
 
-      <p className="">Practice your vocabulary</p>
+        <div className="p-12">
+          <button
+            className="loginLP text-2xl font-medium hover:font-bold"
+            onClick={() => setShowPopupLogin(true)}
+          >
+            Log in
+          </button>
+          {showPopupLogin && (
+            <Popup onClose={() => setShowPopupLogin(false)}>
+              <Login />
+            </Popup>
+          )}
+          <p className="p-2 m-10 text-xl">or</p>
+          <button
+            className="RegisterFirst text-2xl font-medium hover:font-bold"
+            onClick={() => {
+              navigate(`/register`);
+            }}
+          >
+            Sing up
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
